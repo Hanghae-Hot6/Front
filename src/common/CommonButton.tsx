5e7c44b0abef86aa0f713db6dc25f15b2433be16
cho_jae_sin <1005jsc@naver.com>
2022-11-09 16:22:35 +0900

    common button 테스트중

--- src/common/CommonButton.tsx
+++ src/common/CommonButton.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 type CommonButtonProps = {
   path: string;
-  buttonText: string;
+
+  callback?: ({...props}) => void;
+  children?: ReactNode;
 };
 
-const CommonButton = ({path, buttonText}: CommonButtonProps) => {
+const CommonButton = ({
+  path,
+  callback,
+
+  children,
+}: CommonButtonProps) => {
   const navigate = useNavigate();
   const handleClick: React.MouseEventHandler<HTMLButtonElement> = e => {
     e.preventDefault();
 
     navigate(path);
+
+    if (callback) {
+      callback({});
+    }
   };
 
   return (
     <>
-      <button onClick={handleClick}>{buttonText}</button>
+      <button onClick={handleClick}>{children}</button>
     </>
   );
 };
---
+++ src/components/CreateClub/fakeCreateClub.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CommonButton from '../../common/CommonButton';
+import styled from 'styled-components';
+
+type fakeCreateClubProps = {};
+
+const fakeCreateClub = ({}: fakeCreateClubProps) => {
+  const hi = 'hi';
+
+  return (
+    <>
+      <div>아직 가짜 컴포넌트임 ㅋㅋ</div>
+    </>
+  );
+};
+export default fakeCreateClub;
+
+const yesButton = styled.button``;
