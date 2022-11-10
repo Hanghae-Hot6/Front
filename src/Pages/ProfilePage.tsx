29c49f6b59ab428fb1f16892441692b299fcc5ef
kyunghoonkook <rnrrudgg68@gmail.com>
2022-11-10 17:23:06 +0900

    ADD react-query & devtools

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {ReactQueryDevtools} from 'react-query/devtools';
 
 type ProfilePageProps = {};
 
 const ProfilePage = ({}: ProfilePageProps) => {
-  return <></>;
+  return (
+    <>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
 };
 export default ProfilePage;
