52e9875c543daada19ed2918c664a651c6284cd4
cho_jae_sin <1005jsc@naver.com>
2022-11-14 17:03:30 +0900

    Modify ClubListPage 카테고리 로직 mapping 고치기

--- src/Pages/ClubListPage.tsx
+++ src/Pages/ClubListPage.tsx
@@ -44,148 +44,181 @@ const ClubListPage = () => {
     '에세이 시',
   ];
 
-  const categoryTap = [
-    {
-      id: 0,
-      title: '인문',
-      content: data
-        ?.filter((club: clubs) => club.category === '인문')
-        .map((club: clubs) => {
-          /** category에 값이 있을때 map 돌리고 없으면 없다고 출력 */
-          return club.category === '인문' ? (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
+  // [], [sdfsd]
+
+  const categoryTap2 = categoryArray.map((category, index) => {
+    const yes = data?.filter((club: clubs) => club.category === category);
+
+    if (status === 'success') {
+      return {
+        id: index,
+        title: category,
+        // content: data?.filter((club: clubs) => club.category === category),
+        content:
+          yes.length > 0 ? (
+            yes.map((club: clubs) => {
+              /** category에 값이 있을때 map 돌리고 없으면 없다고 출력 */
+              return (
+                <div key={club.id}>
+                  <h2>{club.clubName}</h2>
+                  <p>{club.summary}</p>
+                  <img src={club.thumbnail} alt={club.summary} />
+                </div>
+              );
+            })
           ) : (
             <div>모임이 없습니다</div>
-          );
-        }),
-    },
-    {
-      id: 1,
-      title: '경영 경제',
-      content: data
-        ?.filter((club: clubs) => club.category === '경영 경제')
-        .map((club: clubs) => {
-          return club.category === '경영 경제' ? (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          ) : (
-            <div>모임이 없습니다</div>
-          );
-        }),
-    },
-    {
-      id: 2,
-      title: '자기계발',
-      content: data
-        ?.filter((club: clubs) => club.category === '자기계발')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 3,
-      title: '예술',
-      content: data
-        ?.filter((club: clubs) => club.category === '예술')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 4,
-      title: '자연과학',
-      content: data
-        ?.filter((club: clubs) => club.category === '자연과학')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 5,
-      title: '사회정치',
-      content: data
-        ?.filter((club: clubs) => club.category === '사회정치')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 6,
-      title: 'IT 모바일',
-      content: data
-        ?.filter((club: clubs) => club.category === 'IT 모바일')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 7,
-      title: '소설',
-      content: data
-        ?.filter((club: clubs) => club.category === '소설')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-    {
-      id: 8,
-      title: '에세이 시',
-      content: data
-        ?.filter((club: clubs) => club.category === '에세이 시')
-        .map((club: clubs) => {
-          return (
-            <div key={club.id}>
-              <h2>{club.clubName}</h2>
-              <p>{club.summary}</p>
-              <img src={club.thumbnail} alt={club.summary} />
-            </div>
-          );
-        }),
-    },
-  ];
+          ),
+      };
+    }
+  });
+
+  // console.log(data);
+
+  // const categoryTap = [
+  //   {
+  //     id: 0,
+  //     title: '인문',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '인문')
+  //       .map((club: clubs) => {
+  //         /** category에 값이 있을때 map 돌리고 없으면 없다고 출력 */
+  //         return club.category === '인문' ? (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         ) : (
+  //           <div>모임이 없습니다</div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 1,
+  //     title: '경영 경제',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '경영 경제')
+  //       .map((club: clubs) => {
+  //         return club.category === '경영 경제' ? (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         ) : (
+  //           <div>모임이 없습니다</div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: '자기계발',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '자기계발')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '예술',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '예술')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 4,
+  //     title: '자연과학',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '자연과학')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 5,
+  //     title: '사회정치',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '사회정치')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 6,
+  //     title: 'IT 모바일',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === 'IT 모바일')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 7,
+  //     title: '소설',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '소설')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  //   {
+  //     id: 8,
+  //     title: '에세이 시',
+  //     content: data
+  //       ?.filter((club: clubs) => club.category === '에세이 시')
+  //       .map((club: clubs) => {
+  //         return (
+  //           <div key={club.id}>
+  //             <h2>{club.clubName}</h2>
+
+  //             <p>{club.summary}</p>
+  //             <img src={club.thumbnail} alt={club.summary} />
+  //           </div>
+  //         );
+  //       }),
+  //   },
+  // ];
+
   const [index, setIndex] = useState<number>(0);
 
   return (
@@ -196,23 +229,34 @@ const ClubListPage = () => {
             <section>
               <article>
                 <ul>
-                  {categoryTap?.map(item => (
+                  {/* {categoryTap?.map(item => (
                     <li
                       key={item.id}
                       onClick={() => setIndex(item.id)}
                       className={index === item.id ? 'on' : undefined}>
                       {item.title}
                     </li>
-                  ))}
+                  ))} */}
                 </ul>
               </article>
-              {categoryTap
+              {/* {categoryTap
                 .filter(item => index === item.id)
-                .map(item => (
-                  <>
-                    <div key={item.id}>{item.content}</div>
-                  </>
-                ))}
+                .map(item => {
+                  return (
+                    <>
+                      <div key={item.id}>{item.content}</div>
+                    </>
+                  );
+                })} */}
+              {categoryTap2.length > 0 &&
+                categoryTap2.map((item, index) => {
+                  // console.log(val);
+                  return (
+                    <>
+                      <div key={item?.id}>{item?.content}</div>
+                    </>
+                  );
+                })}
             </section>
           </TabList>
           {/* <ClubListTitle /> */}
