82951e062e6c9dd0340c338ed56d9beb5d19f75a
kyunghoonkook <rnrrudgg68@gmail.com>
2022-12-03 12:23:00 +0900

    Modify review style

---
+++ src/components/ClubDetail/Review.style.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const ReviewCreate = styled.input`
+  width: 50rem;
+  height: 50px;
+  border: 1px solid #ccc;
+  padding: 15px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  :focus {
+    outline: none;
+  }
+`;
+
+export const ReviewWrap = styled.p`
+  margin-top: 15px;
+  padding: 10px;
+  padding-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  > div {
+    border-bottom: 1px dashed #ccc;
+    margin-bottom: 5px;
+    padding-bottom: 8px;
+  }
+  > div {
+    display: flex;
+    justify-content: space-between;
+  }
+  > div > div > small {
+    margin-right: 10px;
+  }
+  > div > div > small:last-child {
+    font-size: 12px;
+    color: #777;
+  }
+`;
+
+export const Btn = styled.button`
+  width: 5rem;
+  height: 2rem;
+  color: #fff;
+  border-radius: 5px;
+  margin-left: 10px;
+  background-color: ${props => props.theme.MainColor};
+  border: 1px solid transparent;
+  transition: all 0.5s;
+  :hover {
+    border: 1px solid ${props => props.theme.MainColor};
+    color: ${props => props.theme.MainColor};
+    background-color: #fff;
+  }
+`;
